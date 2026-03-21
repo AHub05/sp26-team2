87d4dd24eab6bd9739cf76af5471e0e6370e7d0f
Alexander Hubbard <alexanderhubbard05@gmail.com>
2026-03-21 11:29:07 -0400

    Updated dependencies

--- backend-api/src/main/java/com/CSC340/MinervasList/entity/seller.java
+++ backend-api/src/main/java/com/CSC340/MinervasList/entity/seller.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Table;
 public class Seller extends User {
 
     @Id
-    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long sellerId;
 
     @Column(nullable = false)
