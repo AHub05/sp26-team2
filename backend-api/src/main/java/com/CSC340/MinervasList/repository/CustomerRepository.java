45ef2e8418e8c9ad0dab452f3e802c26147dbdcb
Alexander Hubbard <alexanderhubbard05@gmail.com>
2026-03-24 10:51:58 -0400

    Created customer, listing, and review repositories.

--- backend-api/src/main/java/com/CSC340/MinervasList/repository/CustomerRepository.java
+++ backend-api/src/main/java/com/CSC340/MinervasList/repository/CustomerRepository.java
@@ -1,8 +1,12 @@
 package com.CSC340.MinervasList.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.CSC340.MinervasList.entity.Customer;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Customer findByEmail(String email);
+    
 }
--- backend-api/src/main/java/com/CSC340/MinervasList/repository/ListingRepository.java
+++ backend-api/src/main/java/com/CSC340/MinervasList/repository/ListingRepository.java
@@ -1,7 +1,28 @@
 package com.CSC340.MinervasList.repository;
 
 import com.CSC340.MinervasList.entity.Listing;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface ListingRepository extends JpaRepository<Listing, Long> {
+
+    List<Listing> findByName(String name);
+
+    @Query(value = "SELECT l.* FROM listings l WHERE l.seller_id = :sellerId", nativeQuery = true)
+    List<Listing> findBySellerId(Long id);
+
+    @Query(value = "SELECT l.* FROM listing l WHERE l.category = :category", nativeQuery = true)
+    List<Listing> findByCategory(String category);
+
+    @Query(value = "SELECT l.* FROM listing l WHERE l.itemCondition = :itemCondition", nativeQuery = true)
+    List<Listing> findByItemCondition(String condtion);
+
+    @Query(value = "SELECT l.* FROM listing l WHERE l.status = :status", nativeQuery = true)
+    List<Listing> findByStatus(String status);
+
 }
