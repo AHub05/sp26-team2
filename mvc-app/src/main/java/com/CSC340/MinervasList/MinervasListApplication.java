ce5774f43ab36811b6abbbb4d0585f51b48def6a
Alexander Hubbard <alexanderhubbard05@gmail.com>
2026-04-22 10:49:28 -0400

    Created file structure in mvc-app and cleaned up repository.

---
+++ mvc-app/src/main/java/com/CSC340/MinervasList/MinervasListApplication.java
@@ -0,0 +1,13 @@
+package com.CSC340.MinervasList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinervasListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinervasListApplication.class, args);
+	}
+
+}
